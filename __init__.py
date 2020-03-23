b9763c898c3c0ed0194baae28836d760b0ce1ce1
hana bouzid <hana.bouzid95@gmail.com>
2020-03-23 19:04:16 +0100

    first commit

---
+++ __init__.py
@@ -0,0 +1,26 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft import MycroftSkill, intent_file_handler
+import json
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
+
+class RfidReader(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_handler(IntentBuilder("").require("querry"))
+    def handle_login(self):
+        try:
+            id, t = reader.read()
+            self.speak_dialog("login", data={"name": t, "id": id})
+            self.speak_dialog("precaution")
+
+        finally:
+            GPIO.cleanup()
+
+
+def create_skill():
+    return RfidReader()
